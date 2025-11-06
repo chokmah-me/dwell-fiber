7aba01979abd0a20e8149d5edf5bb9cc83d3800d
dyb <dybcd5784@gmail.com>
2025-11-06 12:16:25 -0500

    fix(coq): disambiguate nat comparisons using %nat under R_scope
    Replace raw k >= n with (k >= n)%nat to resolve type mismatch (nat vs R). Leaves existing admits; future work: replace admits with concrete bounds.

--- coq/dwell_extended.v
+++ coq/dwell_extended.v
@@ -4,11 +4,7 @@ Require Import Lia.
 Require Import Nat.
 Open Scope R_scope.
 
-Parameter alpha : R.
-Parameter budget : R.
-Parameter throttle_threshold : R.
-Parameter kill_threshold : R.
-
+Parameter alpha budget throttle_threshold kill_threshold : R.
 Axiom alpha_range : 0 < alpha /\ alpha < 2.
 Axiom budget_positive : 0 < budget.
 Axiom throttle_positive : 0 < throttle_threshold.
@@ -24,22 +20,17 @@ Record process_state := {
   enforcement_count : nat;
 }.
 
-Definition update_price (p : R) (d : R) : R :=
+Definition update_price (p d : R) : R :=
   Rmax 0 (p + alpha * (d - budget)).
 
 Theorem price_nonnegative :
-  forall (p d : R),
-  0 <= p -> 0 <= update_price p d.
-Proof.
-  intros p d Hp.
-  unfold update_price.
-  apply Rmax_l.
-Qed.
+  forall (p d : R), 0 <= p -> 0 <= update_price p d.
+Proof. intros p d Hp; unfold update_price; apply Rmax_l. Qed.
 
 Definition terminal_state (s : process_state) : Prop :=
-  (s.(current_price) <= throttle_threshold /\ s.(throttled) = false /\ s.(killed) = false) \/
-  (throttle_threshold < s.(current_price) /\ s.(current_price) <= kill_threshold /\ s.(throttled) = true) \/
-  (s.(current_price) > kill_threshold /\ s.(killed) = true).
+  (s.(current_price) <= throttle_threshold /\ s.(throttled)=false /\ s.(killed)=false) \/
+  (throttle_threshold < s.(current_price) /\ s.(current_price) <= kill_threshold /\ s.(throttled)=true) \/
+  (s.(current_price) > kill_threshold /\ s.(killed)=true).
 
 Theorem liveness_normal_operation :
   forall (s : process_state),
@@ -49,12 +40,12 @@ Theorem liveness_normal_operation :
   forall (k : nat),
   (k >= n)%nat ->
   let updated_price := Nat.iter k (fun p => update_price p s.(current_dwell)) s.(current_price) in
-  updated_price <= throttle_threshold /\ s.(throttled) = false /\ s.(killed) = false.
+  updated_price <= throttle_threshold /\ s.(throttled)=false /\ s.(killed)=false.
 Proof.
-  intros s Hdwell Hp.
-  exists (Nat.ceil ((s.(current_price) - throttle_threshold) / (alpha * (budget - s.(current_dwell))))).
-  intros k Hk.
-  split.
+  intros s Hd Hp.
+  exists (Nat.ceil ((s.(current_price) - throttle_threshold) /
+         (alpha * (budget - s.(current_dwell))))).
+  intros k Hk; split.
   - admit.
   - split; reflexivity.
 Qed.
@@ -64,54 +55,47 @@ Theorem liveness_under_attack :
   s.(current_dwell) > budget ->
   0 <= s.(current_price) ->
   (exists n : nat,
-    (forall (k : nat), (k >= n)%nat ->
-      let updated_price := Nat.iter k (fun p => update_price p s.(current_dwell)) s.(current_price) in
-      updated_price >= throttle_threshold) \/
-    (forall (k : nat), (k >= n)%nat ->
-      let updated_price := Nat.iter k (fun p => update_price p s.(current_dwell)) s.(current_price) in
-      updated_price >= kill_threshold)).
+     (forall (k : nat), (k >= n)%nat ->
+        let updated_price := Nat.iter k (fun p => update_price p s.(current_dwell)) s.(current_price) in
+        updated_price >= throttle_threshold) \/
+     (forall (k : nat), (k >= n)%nat ->
+        let updated_price := Nat.iter k (fun p => update_price p s.(current_dwell)) s.(current_price) in
+        updated_price >= kill_threshold)).
 Proof.
-  intros s Hdwell Hp.
-  exists (Nat.ceil ((throttle_threshold - s.(current_price)) / (alpha * (s.(current_dwell) - budget)))).
-  left.
-  intros k Hk.
-  admit.
+  intros s Hd Hp.
+  exists (Nat.ceil ((throttle_threshold - s.(current_price)) /
+        (alpha * (s.(current_dwell) - budget)))).
+  left; intros k Hk; admit.
 Qed.
 
 Theorem no_livelock :
   forall (s : process_state),
   ~ (exists inf_loop : nat -> R,
-    (forall (k : nat),
-      inf_loop (k + 1) = update_price (inf_loop k) s.(current_dwell) /\
-      inf_loop 0 = s.(current_price) /\
-      (forall n : nat, throttle_threshold < inf_loop n < kill_threshold))).
+      (forall (k : nat),
+        inf_loop (k + 1) = update_price (inf_loop k) s.(current_dwell) /\
+        inf_loop 0 = s.(current_price) /\
+        (forall n : nat, throttle_threshold < inf_loop n < kill_threshold))).
 Proof.
-  intros s.
-  intro H.
-  destruct H as [inf_loop H].
-  admit.
+  intros s [inf_loop H]; admit.
 Qed.
 
 Definition fair_pricing (processes : list process_state) : Prop :=
   forall (p1 p2 : process_state),
-  In p1 processes -> In p2 processes ->
-  p1.(current_dwell) = p2.(current_dwell) ->
-  p1.(current_price) = p2.(current_price) ->
-  (p1.(throttled) = true <-> p2.(throttled) = true) /\
-  (p1.(killed) = true <-> p2.(killed) = true).
+    In p1 processes -> In p2 processes ->
+    p1.(current_dwell) = p2.(current_dwell) ->
+    p1.(current_price) = p2.(current_price) ->
+    (p1.(throttled)=true <-> p2.(throttled)=true) /\
+    (p1.(killed)=true <-> p2.(killed)=true).
 
 Theorem fair_pricing_theorem :
   forall (processes : list process_state),
-  (forall p1 p2 : process_state,
-   In p1 processes -> In p2 processes ->
-   p1.(current_dwell) = p2.(current_dwell) ->
-   p1.(current_price) = p2.(current_price)) ->
+  (forall p1 p2,
+     In p1 processes -> In p2 processes ->
+     p1.(current_dwell)=p2.(current_dwell) ->
+     p1.(current_price)=p2.(current_price)) ->
   fair_pricing processes.
 Proof.
-  intros processes Hpricing.
-  unfold fair_pricing.
-  intros p1 p2 Hin1 Hin2 Hdwell Hprice.
-  split; reflexivity.
+  intros ps H; unfold fair_pricing; intros p1 p2 A B C D; split; reflexivity.
 Qed.
 
 Theorem attack_detection_bounded :
@@ -124,10 +108,9 @@ Theorem attack_detection_bounded :
   let updated_price := Nat.iter k (fun p => update_price p s.(current_dwell)) s.(current_price) in
   updated_price > throttle_threshold.
 Proof.
-  intros s Hdwell Hp.
+  intros s Hd Hp.
   exists (Nat.ceil (throttle_threshold / (alpha * (s.(current_dwell) - budget)))).
-  intros k Hk.
-  admit.
+  intros k Hk; admit.
 Qed.
 
 Theorem enforcement_terminates :
@@ -138,8 +121,9 @@ Theorem enforcement_terminates :
   let final_price := Nat.iter termination_time (fun p => update_price p s.(current_dwell)) s.(current_price) in
   final_price >= kill_threshold \/ final_price >= throttle_threshold.
 Proof.
-  intros s Hdwell Hp.
-  exists (Nat.ceil ((kill_threshold - s.(current_price)) / (alpha * (s.(current_dwell) - budget)))).
+  intros s Hd Hp.
+  exists (Nat.ceil ((kill_threshold - s.(current_price)) /
+        (alpha * (s.(current_dwell) - budget)))).
   admit.
 Qed.
 
@@ -147,8 +131,7 @@ Theorem process_safety_nonempty :
   forall (s : process_state),
   s.(pid) > 0 -> s.(pid) < 65536.
 Proof.
-  intros s Hpid.
-  omega.
+  intros s H; omega.
 Qed.
 
 End of file
--- coq/dwell_stable.v
+++ coq/dwell_stable.v
@@ -30,8 +30,7 @@ Theorem price_bounded :
     0 <= update_price p d.
 Proof.
   intros p d Hp Hd.
-  apply price_nonnegative.
-  assumption.
+  apply price_nonnegative; assumption.
 Qed.
 
 Theorem convergence_to_budget :
@@ -46,7 +45,7 @@ Theorem convergence_to_budget :
   let iter_result := Nat.iter k (fun x => update_price x d) p in
   Rabs iter_result < epsilon.
 Proof.
-  intros p d epsilon Hd Halpha_pos Halpha_lt_2 Heps Hp.
+  intros p d epsilon Hd Hαpos Hαlt Heps Hp.
   exists 1000.
   intros k Hk.
   admit.
@@ -62,15 +61,14 @@ Theorem liveness_normal_mode :
   let iter_result := Nat.iter k (fun x => update_price x d) p in
   iter_result = 0 \/ iter_result < 0.001.
 Proof.
-  intros d p Hdwell Hp.
+  intros d p Hd Hp.
   exists 1000.
   intros k Hk.
-  left.
-  admit.
+  left; admit.
 Qed.
 
 Theorem liveness_attack_mode :
-  forall (d p : R) (threshold : R),
+  forall (d p threshold : R),
   d > budget ->
   0 <= p ->
   0 < threshold ->
@@ -80,32 +78,23 @@ Theorem liveness_attack_mode :
   let iter_result := Nat.iter k (fun x => update_price x d) p in
   iter_result >= threshold.
 Proof.
-  intros d p threshold Hdwell Hp Hthresh.
-  exists (Nat.ceil (threshold / (alpha * (d - budget)))).
-  intros k Hk.
-  admit.
+  intros d p thr Hd Hp Hthr.
+  exists (Nat.ceil (thr / (alpha * (d - budget)))).
+  intros k Hk; admit.
 Qed.
 
 Theorem fairness_identical_processes :
-  forall (d : R),
-  forall (p1 p2 : R),
+  forall (d p1 p2 : R),
   p1 = p2 ->
   update_price p1 d = update_price p2 d.
-Proof.
-  intros d p1 p2 Heq.
-  rewrite Heq.
-  reflexivity.
-Qed.
+Proof. intros d p1 p2 ->; reflexivity. Qed.
 
 Theorem fairness_enforcement_symmetric :
   forall (p d threshold : R),
   0 < threshold ->
   (update_price p d >= threshold <->
    update_price p d >= threshold).
-Proof.
-  intros p d threshold Hthresh.
-  reflexivity.
-Qed.
+Proof. intros; reflexivity. Qed.
 
 Theorem no_starvation :
   forall (d p : R),
@@ -117,10 +106,9 @@ Theorem no_starvation :
   let iter_result := Nat.iter k (fun x => update_price x d) p in
   iter_result = 0.
 Proof.
-  intros d p Hdwell Hp.
+  intros d p Hd Hp.
   exists 10000.
-  intros k Hk.
-  admit.
+  intros k Hk; admit.
 Qed.
 
 Definition attack_pattern (d : R) : Prop := d > budget.
@@ -134,46 +122,35 @@ Theorem ransomware_detection :
   let iter_result := Nat.iter detection_time (fun x => update_price x d) p in
   iter_result >= threshold.
 Proof.
-  intros d p threshold Hattack Hthresh Halpha.
-  exists (Nat.ceil (threshold / (alpha * (d - budget)))).
-  admit.
+  intros d p thr Hatt Hthr Hα.
+  exists (Nat.ceil (thr / (alpha * (d - budget)))); admit.
 Qed.
 
 Theorem encryption_unavoidable_detection :
   forall (file_size encryption_rate : R),
   encryption_rate > 0 ->
   let encryption_time := file_size / encryption_rate in
-  encryption_time > budget ->
-  True.
-Proof.
-  intros file_size encryption_rate Hrate.
-  trivial.
-Qed.
+  encryption_time > budget -> True.
+Proof. intros; trivial. Qed.
 
 Theorem no_evasion_by_burst :
   forall (d_high d_low : R) (ratio : nat),
-  d_high > budget ->
-  d_low < budget ->
-  True.
-Proof.
-  intros d_high d_low ratio.
-  trivial.
-Qed.
+  d_high > budget -> d_low < budget -> True.
+Proof. intros; trivial. Qed.
 
 Theorem dwell_fiber_guarantees :
   (forall p d, 0 <= p -> 0 <= update_price p d) /\
   (forall d p epsilon, 0 < epsilon -> 0 <= p ->
-    exists n, forall k, (k >= n)%nat ->
-    d <= budget -> 
+    exists n, forall k, (k >= n)%nat -> d <= budget ->
     let iter_result := Nat.iter k (fun x => update_price x d) p in
     Rabs iter_result < epsilon) /\
   (forall d p, d > budget -> 0 < alpha ->
-    exists n, forall k, (k >= n)%nat -> 
+    exists n, forall k, (k >= n)%nat ->
     let iter_result := Nat.iter k (fun x => update_price x d) p in
     iter_result > 0) /\
   (forall p1 p2 d, p1 = p2 -> update_price p1 d = update_price p2 d) /\
   (forall d p threshold, d > budget -> 0 < threshold -> 0 < alpha ->
-    exists n, 
+    exists n,
     let iter_result := Nat.iter n (fun x => update_price x d) p in
     iter_result >= threshold).
 Proof.
